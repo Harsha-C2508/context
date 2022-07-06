065a76f9692c405f1850d708d7a5150d4a948e8c
Harsha-C2508 <harshac2508@gmail.com>
2022-07-06 07:26:43 +0530

    themetoggle

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import { useContext } from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 import { AuthContext } from './context/AuthContext';
+import { ThemeContext } from './context/ThemeContex';
 
 function App() {
   const [state] = useContext(AuthContext)
+  const {isLight} = useContext(ThemeContext)
   return (
-    <div className="App">
+    <div className={`App ${isLight ? "light" : "dark"}`}>
+      <div style={{width:"100%"}}>
       <Navbar/>
       {!state.isAuth && <h1>Please Login and enjoy</h1>}
       {state.isAuth && <div>
@@ -17,6 +20,7 @@ function App() {
         </h2>
       </div>}
     </div>
+      </div>
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useContext } from 'react'
 import { AuthContext } from '../context/AuthContext'
+import { ThemeContext } from '../context/ThemeContex'
 import Styles from '../style/navbar.module.css'
 const Navbar = () => {
     const [state,{ handleLogIn, handleLogOut}] = useContext(AuthContext)
+    const {isLight,toggleTheme} = useContext(ThemeContext)
 
     const handleToggle=()=>{
         if(!state.isAuth){
@@ -22,6 +24,12 @@ const Navbar = () => {
             <h3>Mens</h3>
             <h3>Cart</h3>
             <button onClick={handleToggle}>{state.isAuth? "LogOut" : "LogIn" }</button>
+            <button 
+            className={Styles.button} 
+            onClick={toggleTheme}>
+              {` ${
+                isLight ? "Dark":"Light"
+              }`}</button>
         </div>
     </div>
   )
